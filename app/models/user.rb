a5a4b47818be23f4c48c2cb8f86a0e50cf3895d4
Un Se Pyon <Un_Se_Pyon@mckinsey.com>
2014-10-08 09:41:45 -0400

    Added validation for max number of characters for first/last/user names, added toggle buttons to hide user moves in rounds, fixed typo in About Us

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates_length_of :user_name, :maximum => 16
+  validates_length_of :first_name, :maximum => 16
+  validates_length_of :last_name, :maximum => 16
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :user_name, presence: true
