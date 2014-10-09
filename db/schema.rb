a200bcb40088ed845983c6ab7666300ed4719f77
Un Se Pyon <Un_Se_Pyon@mckinsey.com>
2014-10-09 09:54:37 -0400

    Added FAQ page, changed some CSS for the page, when hovering over textfields in users_new a descritpion will appear. Thinking about creating a blog page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003185035) do
+ActiveRecord::Schema.define(version: 20141008173649) do
+
+  create_table "faqs", force: true do |t|
+    t.string   "question"
+    t.text     "answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "games", force: true do |t|
     t.integer  "user_1_id",                        null: false
