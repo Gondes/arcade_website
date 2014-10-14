2d21b8e180ad977ae5a78bae5e14de81a5080383
Un Se Pyon <Un_Se_Pyon@mckinsey.com>
2014-10-14 09:07:24 -0400

    Increased first/last/user name character limit from 16 to 24.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,13 @@
 class User < ActiveRecord::Base
-  validates_length_of :user_name, :maximum => 16
-  validates_length_of :first_name, :maximum => 16
-  validates_length_of :last_name, :maximum => 16
+  validates_length_of :user_name, :maximum => 24
+  validates_length_of :first_name, :maximum => 24
+  validates_length_of :last_name, :maximum => 24
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :user_name, presence: true
 
   def name
     "#{user_name}"
-  end
-
-  def init
-    self.wins_count          ||= 0      #will set the default value only if it's nil
-    self.loss_count          ||= 0
-    self.tie_count           ||= 0
-    self.games_played_count  ||= 0
-    self.best_win_streak     ||= 0
-    self.current_win_streak  ||= 0
   end
 
   def wins
