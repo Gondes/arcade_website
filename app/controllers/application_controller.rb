770f8bf63310c2586d90ffbf15fad72eb8dc4305
Un Se Pyon <Un_Se_Pyon@mckinsey.com>
2014-10-16 16:17:25 -0400

    Made changes to views to better integrate login. Added images and started to edit the rock_paper_scissor_round forms to incorporate 2 player capabilities from 2 remote computers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if current_account.user_id.nil?
+      new_user_path
+    else
+      user_path(current_account.user_id)
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    "/"
+  end
 end
