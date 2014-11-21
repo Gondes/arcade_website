8c790f81c4dd4dbd92448756271b6e17757481bf
Un Se Pyon <Un_Se_Pyon@mckinsey.com>
2014-11-21 15:46:21 -0500

    Making factories.rb look cleaner.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,26 @@
 FactoryGirl.define do
 
+  factory :comment do
+    content "MyText"
+    discussion_topic_id 1
+    user_id 1
+    removed false
+  end
+
+  factory :discussion_topic do
+    title "MyString"
+    description "MyText"
+    general_forum_topic_id 1
+    creator_id 1
+    closed false
+    removed false
+  end
+
+  factory :general_forum_topic do
+    title "MyString"
+	description "MyText"
+  end
+
   factory :faq do
     question "Some title"
     answer "Some answer."
