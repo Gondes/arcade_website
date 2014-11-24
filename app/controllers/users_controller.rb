35345810e0865037f437556de2500aa06dafb5db
Un Se Pyon <Un_Se_Pyon@mckinsey.com>
2014-11-24 12:38:44 -0500

    Added authenticate_user! check for user and rps_rounds controllers and adjusted their specs accordingly.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy, :reset_stats]
 
   helper_method :sort_column, :sort_direction
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,9 +11,11 @@ describe UsersController do
 
   describe "index" do
     it "GET index" do
+      sign_in @my_user
       get :index
       expect(response).to be_success
       response.status.should be(200)
+      sign_out @my_user
     end
   end
 
@@ -28,7 +30,7 @@ describe UsersController do
 
     it "if not logged in, it should redirect to users_path" do
       get :show, :id => @my_user.id
-      response.should redirect_to users_path
+      response.should redirect_to new_user_session_path
     end
   end
 
@@ -47,7 +49,7 @@ describe UsersController do
 
     it "if not logged in, it should redirect to users_path", type: :request do
       get :edit, :id => @my_user.id
-      response.should redirect_to users_path
+      response.should redirect_to new_user_session_path
     end
   end
 
@@ -66,21 +68,25 @@ describe UsersController do
 
   describe "destroy" do
     it "DELETE user" do
+      sign_in @my_user
       account = create(:user, :user_name => "Fire", :email => "ricky.flame@example.com")
       expect((User.find account.id).user_name).should eq(account.user_name)
       expect { delete :destroy, :id => account }.should change(User, :count)
       lambda { User.find account.id }.should raise_error(ActiveRecord::RecordNotFound)
+      sign_out @my_user
     end
   end
 
   describe "update" do
     it "PUT user" do
+      sign_in @my_user
       attributes = attributes_for(:user, :user_name => "un", :first_name => "fn", :last_name => "ln")
       put :update, :id => @my_user, :user => attributes
       @my_user.reload
       @my_user.user_name.should eq(attributes[:user_name])
       @my_user.first_name.should eq(attributes[:first_name])
       @my_user.last_name.should eq(attributes[:last_name])
+      sign_out @my_user
     end
 
     it "PUT should not update invalid user_name user, empty" do
@@ -116,12 +122,14 @@ describe UsersController do
 
   describe "reset_stats" do
     it "should reset user's stats and save" do
+      sign_in @my_user
       account = create(:user, :user_name => "Fire", :email => "ricky.flame@example.com", :games_played_count => 1)
       account.games_played_count.should eq(1)
       put :reset_stats, :id => account
       account.reload
       account.games_played_count.should eq(0)
       delete :destroy, :id => account
+      sign_out @my_user
       # I am assuming that testing one column should be sufficient to prove this action works
       #put :reset_stats, :id => @my_user
       #expect(response).to be_success
