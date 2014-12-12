27c66882a9fe7246c0ed39b1d64829a0358046a4
Un Se Pyon <Un_Se_Pyon@mckinsey.com>
2014-12-12 10:41:53 -0500

    Made some changes to list page navigation by removing unneeded params and making methods flexable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,21 +5,35 @@ module ApplicationHelper
     link_to title, :sort => column, :direction => direction
   end
 
-  def next_page(page = 1, user_id = nil)
+  def next_page(title = nil)
     #fill out this helper to navigate pages.
-    if user_id.nil?
-      link_to "Next", :page => page + 1
-    else
-      link_to "Next", :page => page + 1, :user_id => user_id
+    link_name = "Next"
+    if title.nil?
+      link_to link_name, :page => params[:page].to_i + 1
+    elsif title == "games"
+      #link_to "Next", :page => page + 1, :user_id => some_id
+      link_to link_name, :page => params[:page].to_i + 1, :user_id => params[:user_id].to_i
+    elsif title == "topics"
+      "topic"
+    elsif title == "comments"
+      "comment"
+    elsif title == "users"
+      "user"
     end
   end
 
-  def previous_page(page = 1, user_id = nil)
-    #fill out this helper to navigate pages.
-    if user_id.nil?
-      link_to "Previous", :page => page - 1
-    else
-      link_to "Previous", :page => page - 1, :user_id => user_id
+  def previous_page(title = nil)
+    link_name = "Previous"
+    if title.nil?
+      link_to link_name, :page => params[:page].to_i - 1
+    elsif title == "games"
+      link_to link_name, :page => params[:page].to_i - 1, :user_id => params[:user_id].to_i
+    elsif title == "topics"
+      "topic"
+    elsif title == "comments"
+      "comment"
+    elsif title == "users"
+      "user"
     end
   end
 end
